fdf2738566d22bebeeaa26d5433355baab3c68f8
Brian Cooper <brian@brian-cooper.com>
2025-11-13 17:59:43 -0800

    feature: add base files

---
+++ app/_layout.tsx
@@ -0,0 +1,8 @@
+import { Stack } from "expo-router";
+
+/**
+ * Root layout.
+ */
+const RootLayout = () => <Stack />;
+
+export default RootLayout;
---
+++ app/index.tsx
@@ -0,0 +1,18 @@
+import { Text, View } from "react-native";
+
+/**
+ * Index page.
+ */
+const Index = () => (
+  <View
+    style={{
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+    }}
+  >
+    <Text>🦦</Text>
+  </View>
+);
+
+export default Index;
